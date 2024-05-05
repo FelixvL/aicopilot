724326f87ca9de0a946caaf4f2e837288720a2db
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-05-05 23:02:30 +0200

    volgende commit

--- _runalles.sh
+++ _runalles.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "Huidige datum en tijd:"
-date
-asciidoctor index.adoc
-asciidoctor 4006curriculum.adoc
-asciidoctor 4006labs.adoc
+fswatch -o /Users/lonlrn/Desktop/AIDocumentatie4006/asciidocsite/4006labs.adoc | while read f
+do
+    echo "Bestand is gewijzigd, script wordt uitgevoerd..."
+    echo "Huidige datum en tijd:"
+    date
+    asciidoctor index.adoc
+    asciidoctor 4006curriculum.adoc
+    asciidoctor 4006labs.adoc
 
-git add .
-git commit -m "volgende commit"
-git pull
-git push
+    git add .
+    git commit -m "volgende commit"
+    git pull
+    git push
+done
